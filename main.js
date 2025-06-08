d234f342082ba862e32a54380967b5d588b5a442
Ado <adonaylester85@gmail.com>
2025-06-08 10:58:16 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-Process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1'
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1'
 import './config.js';
 import { createRequire } from 'module'
 import path, { join } from 'path'
