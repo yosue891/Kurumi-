b210f4c191ffafd704e23ef90283b468e5525172
Ado <adonaylester85@gmail.com>
2025-06-09 14:07:26 -0600

    Actualizar formarpareja.js

--- plugins/formarpareja.js
+++ plugins/formarpareja.js
@@ -13,6 +13,5 @@ handler.help = ['formarpareja'];
 handler.tags = ['fun'];
 handler.command = ['formarpareja', 'formarparejas'];
 handler.group = true;
-handler.register = true;
 
 export default handler;
