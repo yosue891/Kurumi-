757a544768f5c998995ab7719687f630bf430ff3
Ado <adonaylester85@gmail.com>
2025-06-08 21:07:35 -0600

    Actualizar dl-githubsearch.js

--- plugins/dl-githubsearch.js
+++ plugins/dl-githubsearch.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 
-if (!text) return conn.reply(m.chat, `${emoji} Por favor ingresa un nombre de un repositorio GitHub.`, m);
+if (!text) return conn.reply(m.chat, `Por favor ingresa un nombre de un repositorio GitHub.`, m);
 
 try {
 let api = `https://dark-core-api.vercel.app/api/search/github?key=api&text=${text}`;
