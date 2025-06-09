45f3dc926868edc3d3f3979c36c40a940a07f099
Ado <adonaylester85@gmail.com>
2025-06-08 21:38:56 -0600

    Actualizar tools-ssweb.js

--- plugins/tools-ssweb.js
+++ plugins/tools-ssweb.js
@@ -1,14 +1,14 @@
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, command, args }) => {
-if (!args[0]) return conn.reply(m.chat, `🍁 Por favor, ingrese el Link de una página.`, m)
+if (!args[0]) return conn.reply(m.chat, `🍁 Por favor, ingrese el Link de una página.`, m, rcanal)
 try {
 await m.react(rwait)
 let ss = await (await fetch(`https://image.thum.io/get/fullpage/${args[0]}`)).buffer()
 conn.sendFile(m.chat, ss, 'error.png', args[0], m)
 await m.react(done)
 } catch {
-return conn.reply(m.chat, `Ocurrió un error.`, m)
+return conn.reply(m.chat, `Ocurrió un error.`, m, rcanal)
 await m.react(error)}}
 
 handler.help = ['ssweb']
