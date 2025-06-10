804fd980df1f2797e983582629ab4370176c2f61
Ado <adonaylester85@gmail.com>
2025-06-09 21:32:13 -0600

    Actualizar tools-toimg.js

--- plugins/tools-toimg.js
+++ plugins/tools-toimg.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
     if (!/sticker/.test(mime)) return m.reply(notStickerMessage)
     const media = await q.download()
     let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
-    await conn.sendFile(m.chat, out, 'output.png', null, m, rcanal)
+    await conn.sendFile(m.chat, out, 'output.png', null, m)
 }
 
 handler.help = ['toimg (reply)']
