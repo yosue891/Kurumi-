d1efe370d5fd6ab92079f3d6721e982bd8ad9792
Ado <adonaylester85@gmail.com>
2025-06-09 21:31:18 -0600

    Crear tools-toimg.js

---
+++ plugins/tools-toimg.js
@@ -0,0 +1,17 @@
+import { webp2png } from '../lib/webp2mp4.js'
+
+let handler = async (m, { conn, usedPrefix, command }) => {
+    const notStickerMessage = `> 🍟 Debes citar un sticker para convertir a imagen.`
+    const q = m.quoted || m
+    const mime = q.mediaType || ''
+    if (!/sticker/.test(mime)) return m.reply(notStickerMessage)
+    const media = await q.download()
+    let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
+    await conn.sendFile(m.chat, out, 'output.png', null, m, rcanal)
+}
+
+handler.help = ['toimg (reply)']
+handler.tags = ['tools']
+handler.command = ['toimg', 'img', 'jpg']
+
+export default handler
