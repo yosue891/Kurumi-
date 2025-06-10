2c5babc4049a4ac44d841365fe607f81a1743ae9
Ado <adonaylester85@gmail.com>
2025-06-09 21:33:36 -0600

    Actualizar tools-toimg.js

--- plugins/tools-toimg.js
+++ plugins/tools-toimg.js
@@ -1,13 +1,23 @@
 import { webp2png } from '../lib/webp2mp4.js'
+import { fromBuffer } from 'file-type'
 
-let handler = async (m, { conn, usedPrefix, command }) => {
-    const notStickerMessage = `> 🍟 Debes citar un sticker para convertir a imagen.`
-    const q = m.quoted || m
-    const mime = q.mediaType || ''
-    if (!/sticker/.test(mime)) return m.reply(notStickerMessage)
-    const media = await q.download()
-    let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
-    await conn.sendFile(m.chat, out, 'output.png', null, m)
+let handler = async (m, { conn }) => {
+  const notStickerMessage = `> 🍟 Debes citar un *sticker fijo* para convertir a imagen.`
+  const q = m.quoted || m
+  const mime = q.mediaType || ''
+  
+  if (!/sticker/.test(mime)) return m.reply(notStickerMessage)
+  
+  const media = await q.download()
+  const fileType = await fromBuffer(media)
+  
+  if (!fileType || fileType.ext !== 'webp') return m.reply(`❌ Solo se puede convertir stickers fijos (no animados).`)
+  
+  let out = await webp2png(media).catch(_ => null)
+  
+  if (!out) return m.reply(`❌ Error al convertir el sticker.`)
+  
+  await conn.sendFile(m.chat, out, 'imagen.png', null, m)
 }
 
 handler.help = ['toimg (reply)']
